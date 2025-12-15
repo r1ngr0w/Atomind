1664a107902cb68d78c07cca51ca0804ca4fe75d
Michele <michelepassariello@live.it>
2025-12-15 15:51:17 +0100

    Initial Commit

---
+++ Atomind/AtomindApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomindApp.swift
+//  Atomind
+//
+//  Created by AFP FED 06 on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AtomindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
